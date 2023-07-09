fb9c639a88d10bda81b85b3fdab847899f2ed70f
Pootispower <ale45883412@yandex.ru>
2023-07-09 10:16:00 +0700

    правки 3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv(
 
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',

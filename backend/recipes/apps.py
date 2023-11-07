e44f30f52db73e251b3e387e741c35c449874ede
Комков Алексей <ale45883412@yandex.ru>
2023-11-07 17:47:47 +0700

    Добавил verbose_name

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -1,11 +1,12 @@
 """
-Module for configuring the 'recipes' application.
+Module for configuring the "recipes" application.
 """
 from django.apps import AppConfig
 
 
 class RecipesConfig(AppConfig):
-    """ Class for configuring the 'recipes' application. """
+    """Class for configuring the "recipes" application."""
 
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "recipes"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recipes'
+    verbose_name = 'Recipe catalogue.'

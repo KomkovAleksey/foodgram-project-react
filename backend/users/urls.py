21bb362c7bde46220d43e2483cc7cd09d076c279
Комков Алексей <ale45883412@yandex.ru>
2023-11-07 20:22:01 +0700

    CustomUserViewSet

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,21 +1,17 @@
 """
-Module for 'users' app routes
+Module for "users" app routes
 """
 from django.urls import include, path
 
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-from .views import SubscribeViewSet
-
-app_name = 'users'
-
-router_v1 = SimpleRouter()
+from .views import CustomUserViewSet
 
 # foodgram router_v1 users v.1
+router_v1 = DefaultRouter()
 
-router_v1.register(
-    'users/<int:pk>/subscribe/', SubscribeViewSet, basename='subscribe'
-    )
+
+router_v1.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router_v1.urls)),

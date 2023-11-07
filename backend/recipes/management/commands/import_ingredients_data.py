efdb7732211f73c78b71e2681a809df5ce8e307f
Комков Алексей <ale45883412@yandex.ru>
2023-11-07 17:04:09 +0700

    Дополнил команду для импорта ингридиентов в базу данных.

--- backend/recipes/management/commands/import_ingredients_data.py
+++ backend/recipes/management/commands/import_ingredients_data.py
@@ -1,3 +1,7 @@
+"""
+Custom django-admin command to load data from csv files.
+"""
+import os
 import csv
 
 from django.core.management.base import BaseCommand
@@ -7,24 +11,30 @@ from recipes.models import Ingredient
 
 class Command(BaseCommand):
     """
-    Команда для импорта данных из файла csv в базу данных:
-    python manage.py import_ingredients_data
-    команда в докер контейнере:
+    Command to import data from csv file into databases:
+    python Manage.py import_ingredients_data
+    in a docker container:
     docker compose exec backend python manage.py import_ingredients_data
     """
 
-    help = "Заполняем базу данных ингридиентов."
+    help = 'Filling out the ingredients database.'
 
     def handle(self, **kwargs):
-        with open(
-                './data/ingredients.csv', encoding='utf-8'
-        ) as file:
-            reader = csv.reader(file, delimiter=",")
-            for row in reader:
-                Ingredient.objects.get_or_create(
-                    name=row[0],
-                    measurement_unit=row[1]
-                )
-        self.stdout.write(
-            self.style.SUCCESS("[+]***Данные ингридиентов загружены!***")
+        starting_message = (
+            '***Starting filling out the ingredients database!!!***'
         )
+        self.stdout.write(self.style.WARNING(f'{starting_message}'))
+        ingredients_file_path = './data/ingredients.csv'
+        if os.path.exists(ingredients_file_path):
+            with open(ingredients_file_path, encoding='utf-8') as file:
+                reader = csv.reader(file, delimiter=',')
+                for row in reader:
+                    Ingredient.objects.get_or_create(
+                        name=row[0],
+                        measurement_unit=row[1]
+                    )
+            self.stdout.write(self.style.SUCCESS('***Ingredient data loaded!***'))
+        else:
+            self.stdout.write(
+                self.style.ERROR('***File ingredients.csv not found!***')
+            )

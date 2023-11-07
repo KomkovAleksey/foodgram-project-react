e2a6a2a4edc4867a5a063cbcec9c0a805ad35292
Комков Алексей <ale45883412@yandex.ru>
2023-11-07 17:13:25 +0700

    Настроил админку

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,25 +1,65 @@
 """
-Module for registering 'users' app models in the admin interface.
+A module for registering and configuring "users"
+application models in the administrator interface.
 """
 from django.contrib import admin
 
-from .models import CustomUser, Subscribe
+from .models import CustomUser, Follow
+
+
+admin.site.site_header = (
+    'Administration of the "users" app for "foodgram" project.'
+)
+
+admin.site.empty_value_display = '-empty-'
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    """ Represents the 'CustomUser' model in the admin interface. """
+    """Represents the "CustomUser" model in the admin interface."""
 
-    list_display = ('pk', 'username', 'first_name', 'last_name', 'email',)
-    search_fields = ('username', 'email', 'first_name', 'last_name',)
-    list_filter = ('username', 'email', 'first_name', 'last_name',)
-    empty_value_display = '-empty-'
+    list_display = (
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'recipe_count',
+        'follower_count',
+    )
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+    list_filter = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+
+    @admin.display(description='Number of recipes in the favorites list')
+    def recipe_count(self, obj):
+        return obj.recipes.count()
+
+    @admin.display(description='Number of followers')
+    def follower_count(self, obj):
+        return obj.author_followers.count()
 
 
-@admin.register(Subscribe)
-class SubscriptionAdmin(admin.ModelAdmin):
-    """ Represents the 'Subscribe' model in the admin interface. """
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    """Represents the "Subscribe" model in the admin interface."""
 
-    list_display = ('pk', 'user', 'following',)
-    list_filter = ('user', 'following',)
-    empty_value_display = '-empty-'
+    list_display = (
+        'user',
+        'author',
+    )
+    list_filter = (
+        'user',
+        'author',
+    )
+    ordering = (
+        'id',
+    )

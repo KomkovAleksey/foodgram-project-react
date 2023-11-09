b1ba4364153fc07b7e6f6e747779ea996a244ffe
Комков Алексей <ale45883412@yandex.ru>
2023-11-10 00:43:12 +0700

    добавил ettext_lazy

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -1,47 +1,48 @@
 """
 Constants.
 """
+from django.utils.translation import gettext_lazy as _
 
 
 class Help_text_recipes():
     """Help text for recipes app models in admin zone."""
 
     # Ingredient model.
-    INGREDIENT_NAME = 'Enter a unique ingredient name.'
+    INGREDIENT_NAME = _('Enter a unique ingredient name.')
 
-    MEASUREMENT_UNIT = 'Enter the unit of measurement for the ingredient.'
+    MEASUREMENT_UNIT = _('Enter the unit of measurement for the ingredient.')
 
     # Tag model.
-    TAG_NAME = 'Enter a unique tag name.'
+    TAG_NAME = _('Enter a unique tag name.')
 
-    TAG_COLOR = 'Select a tag color.'
+    TAG_COLOR = _('Select a tag color.')
 
-    TAG_SLUG = 'Enter a unique tag slug.'
+    TAG_SLUG = _('Enter a unique tag slug.')
 
     # Recipe model.
-    RECIPE_NAME = 'Enter a unique recipe name.'
+    RECIPE_NAME = _('Enter a unique recipe name.')
 
-    RECIPE_AUTHOR = 'Enter the unique name of the recipe author.'
+    RECIPE_AUTHOR = _('Enter the unique name of the recipe author.')
 
-    RECIPE_TEXT = 'Add a recipe description.'
+    RECIPE_TEXT = _('Add a recipe description.')
 
-    RECIPE_TAG = 'Add recipe tags.'
+    RECIPE_TAG = _('Add recipe tags.')
 
-    RECIPE_INGREDIENT = 'Add ingredients to recipe.'
+    RECIPE_INGREDIENT = _('Add ingredients to recipe.')
 
-    RECIPE_COOKING_TIME = 'Enter the cooking time for the recipe in minutes.'
+    RECIPE_COOKING_TIME = _('Enter the cooking time for the recipe in minutes.')
 
 
 class Help_text_users():
     """Help text for users app models in admin zone."""
 
     # CustomUser model.
-    HELP_EMAIL = "Enter the user's unique email address."
+    HELP_EMAIL = _("Enter the user's unique email address.")
 
-    HELP_PASSWORD = 'Enter your unique user password.'
+    HELP_PASSWORD = _('Enter your unique user password.')
 
-    HELP_USERNAME = 'Enter a unique username.'
+    HELP_USERNAME = _('Enter a unique username.')
 
-    HELP_FIRST_NAME = 'Enter your name.'
+    HELP_FIRST_NAME = _('Enter your name.')
 
-    HELP_LAST_NAME = 'Enter your last name.'
+    HELP_LAST_NAME = _('Enter your last name.')

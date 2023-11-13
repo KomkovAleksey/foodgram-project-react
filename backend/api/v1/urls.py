f97e4ee54e76bff16b7fe79de391f5d738237818
Комков Алексей <ale45883412@yandex.ru>
2023-11-14 01:38:01 +0700

    fix

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,24 +1,25 @@
 """
-Module for 'api' app routes
+URL configuration for "api" app.
 """
-from rest_framework.routers import SimpleRouter
-
 from django.urls import include, path
+from rest_framework import routers
 
-from .views import IngredientViewSet, TagViewSet, RecipeViewSet
+from .views import (
+    IngredientsViewSet,
+    RecipeViewSet,
+    TagViewSet,
+)
 
-app_name = 'api'
 
-router_v1 = SimpleRouter()
+router_v1 = routers.DefaultRouter()
 
-# foodgram router_v1 API v.1
+router_v1.register("tags", TagViewSet, basename='tags')
+router_v1.register("recipes", RecipeViewSet, basename='recipes')
+router_v1.register("ingredients", IngredientsViewSet, basename='ingredients')
 
-router_v1.register('tags', TagViewSet, basename='tags')
-router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
-router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
-    path('', include(router_v1.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path("", include(router_v1.urls)),
+    path("", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]

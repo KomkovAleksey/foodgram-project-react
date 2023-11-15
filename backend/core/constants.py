6b42bc63c6fadb82634fea0e31c3f4797463bfb4
Комков Алексей <ale45883412@yandex.ru>
2023-11-15 09:27:19 +0700

    Const_users, Const_recipes.

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -9,28 +9,22 @@ class Help_text_recipes():
 
     # Ingredient model.
     INGREDIENT_NAME = _('Enter a unique ingredient name.')
-
     MEASUREMENT_UNIT = _('Enter the unit of measurement for the ingredient.')
-
     # Tag model.
     TAG_NAME = _('Enter a unique tag name.')
-
     TAG_COLOR = _('Select a tag color.')
-
     TAG_SLUG = _('Enter a unique tag slug.')
-
     # Recipe model.
     RECIPE_NAME = _('Enter a unique recipe name.')
-
     RECIPE_AUTHOR = _('Enter the unique name of the recipe author.')
-
     RECIPE_TEXT = _('Add a recipe description.')
-
     RECIPE_TAG = _('Add recipe tags.')
-
     RECIPE_INGREDIENT = _('Add ingredients to recipe.')
-
-    RECIPE_COOKING_TIME = _('Enter the cooking time for the recipe in minutes.')
+    RECIPE_COOKING_TIME = _(
+        'Enter the cooking time for the recipe in minutes.'
+    )
+    # IngredientInRecipe model.
+    AMOUNT = _('Please indicate the quantity of ingredients.')
 
 
 class Help_text_users():
@@ -38,11 +32,32 @@ class Help_text_users():
 
     # CustomUser model.
     HELP_EMAIL = _("Enter the user's unique email address.")
-
     HELP_PASSWORD = _('Enter your unique user password.')
-
     HELP_USERNAME = _('Enter a unique username.')
-
     HELP_FIRST_NAME = _('Enter your name.')
-
     HELP_LAST_NAME = _('Enter your last name.')
+
+
+class Const_users():
+    """Constants for users app models."""
+
+    # CustomUser model.
+    MAX_EMAIL_LENGTH = 254
+    MAX_USER_LENGTH = 150
+    MAX_PASSWORD_LENGTH = 50
+
+
+class Const_recipes():
+    """Constants for recipes app models."""
+
+    MAX_NAME_LENGTH = 256
+    # Ingredient model.
+    MAX_MEASUREMENT_UNIT = 256
+    # Tag model.
+    MAX_SLUG_LENGTH = 64
+    # Recipe model.
+    MIN_COOKING_TIME = 1
+    MAX_COOKING_TIME = 9000
+    # IngredientInRecipe model.
+    MIN_AMOUNT = 1
+    MAX_AMOUNT = 9000

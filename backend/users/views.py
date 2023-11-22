6110c20b7cc9546b6ab021adb34b3268d347b555
Комков Алексей <ale45883412@yandex.ru>
2023-11-23 03:40:35 +0700

    Переделал подписки

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,13 +4,14 @@ Module for creating, configuring and managing `users' app viewsets
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Follow
+from api.v1.pagination import CustomPagination
+from api.v1.permissions import IsAdminUserOrReadOnly
 from .serializers import (
     CustomUserSerializer,
     SubscriptionSerializer
@@ -19,16 +20,13 @@ from .serializers import (
 User = get_user_model()
 
 
-def get_author(id):
-    return get_object_or_404(User, id=id)
-
-
 class CustomUserViewSet(UserViewSet):
     """Vievset for working with users."""
 
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPagination
+    permission_classes = (IsAdminUserOrReadOnly,)
 
     @action(
         detail=False,
@@ -37,55 +35,60 @@ class CustomUserViewSet(UserViewSet):
     )
     def subscriptions(self, request):
         subscribed_to = self.paginate_queryset(
-            User.objects.filter(author__follower=request.user)
+            User.objects.filter(following__user=request.user)
         )
-        serializer = SubscriptionSerializer(subscribed_to, many=True)
+        serializer = SubscriptionSerializer(
+            subscribed_to,
+            many=True,
+            context={"request": request}
+        )
         return self.get_paginated_response(serializer.data)
 
     @action(
         detail=True,
-        methods=['post'],
+        methods=['post', 'delete'],
         permission_classes=[IsAuthenticated],
     )
     def subscribe(self, request, id):
-        """ Subscribe to the user. """
-        author = get_author(id)
-        follower = request.user
+        user = request.user
+        author = get_object_or_404(User, pk=id)
+        Subscription = Follow.objects.filter(user=user, author=author)
 
-        serializer = SubscriptionSerializer(
-            author,
-            data=request.data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save
-
-        subscription, created = Follow.objects.get_or_create(
-            follower=follower,
-            author=author,
-        )
-
-        if created:
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
+        if request.method == 'POST':
+            serializer = SubscriptionSerializer(
+                author, data=request.data, context={'request': request}
             )
-        else:
-            message = {'You are already following this author'}
+            serializer.is_valid(raise_exception=True)
+            if Subscription.exists():
+                return Response(
+                    {'subscribe': 'You are already subscribed to this user.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if user == author:
+                return Response(
+                    {'subscribe': "You can't subscribe to yourself."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            Follow.objects.create(user=user, author=author)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+        if request.method == 'DELETE':
+            if not Subscription.exists():
+                return Response(
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            Subscription.delete()
             return Response(
-                message, status=status.HTTP_400_BAD_REQUEST
+                status=status.HTTP_204_NO_CONTENT
             )
 
-    @subscribe.mapping.delete
-    def delete_subscribe(self, request, id):
-        """Unsubscribe from a user."""
-        author = get_author(id)
-        subscription = get_object_or_404(
-            Follow,
-            follower=request.user,
-            author=get_author(id),
-        )
-        subscription.delete()
-        message = {f'You have unsubscribed from this {author}'}
-
-        return Response(message, status=status.HTTP_204_NO_CONTENT)
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[IsAuthenticated],
+    )
+    def me(self, request):
+        """View subscriptions to authors. My subscriptions."""
+        user = request.user
+        serializer = CustomUserSerializer(user, context={"request": request})
+        return Response(serializer.data, status=status.HTTP_200_OK)

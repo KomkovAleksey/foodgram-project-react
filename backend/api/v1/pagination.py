af4b9ca2e3f8e64729c464dc9698e7aa460eb47d
Комков Алексей <ale45883412@yandex.ru>
2023-11-23 00:41:27 +0700

    fix

--- backend/api/v1/pagination.py
+++ backend/api/v1/pagination.py
@@ -7,5 +7,5 @@ from rest_framework import pagination
 class CustomPagination(pagination.PageNumberPagination):
     """A paginator that divides data into pages."""
 
+    page_size_query_param = "limit"
     page_size = 6
-    page_size_query_param = 'page_size'

9992b808d040a317b507bc84b641495ae6df44db
Комков Алексей <ale45883412@yandex.ru>
2023-11-25 00:51:36 +0700

    RegexUsername,

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,27 +7,11 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 from core.constants import HelpTextUsers, ConstantUsers
-from core.validators import validate_username
+from core.validators import validate_username, RegexUsername
 
 
 class CustomUser(AbstractUser):
     """CustomUser model class."""
-
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-    CUSTOM_USER_ROLE_CHOICES = [
-        (USER, 'user'),
-        (MODERATOR, 'moderator'),
-        (ADMIN, 'admin'),
-    ]
-
-    role = models.CharField(
-        verbose_name='User Role',
-        choices=CUSTOM_USER_ROLE_CHOICES,
-        default=USER,
-        max_length=16,
-    )
 
     email = models.EmailField(
         verbose_name='Email address',
@@ -42,10 +26,7 @@ class CustomUser(AbstractUser):
         help_text=HelpTextUsers.HELP_USERNAME,
         validators=(
             validate_username,
-            RegexValidator(
-                regex=r'^[\w.@+-]+$',
-                message='The username contains invalid characters.',
-            ),
+            RegexUsername,
         )
     )
     first_name = models.CharField(
@@ -83,14 +64,6 @@ class CustomUser(AbstractUser):
                 name='unique_username_email'
             )
         ]
-
-    @property
-    def is_admin_or_super_user(self):
-        return self.role == self.ADMIN or self.is_superuser
-
-    @property
-    def is_moderator(self):
-        return self.role == self.MODERATOR
 
     def __str__(self):
         return f'{self.get_full_name()}'

00f0dd31dfe5567c0731412a12636530c376621e
Комков Алексей <ale45883412@yandex.ru>
2023-11-25 20:21:07 +0700

    умолчательное значение

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -83,12 +83,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 if not DEVELOP:
     DATABASES = {
         "default": {
-            "ENGINE": os.getenv("DB_ENGINE"),
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT")
+            "ENGINE": os.getenv("DB_ENGINE", default='django.db.backends.postgresql'),
+            "NAME": os.getenv("DB_NAME", default='postgres'),
+            "USER": os.getenv("POSTGRES_USER", default='postgres'),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", default='postgres'),
+            "HOST": os.getenv("DB_HOST", default='db'),
+            "PORT": os.getenv("DB_PORT", default='5432')
         }
     }
 else:

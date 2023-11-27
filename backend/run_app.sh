1f74487de4797889883fdac39ddf4e7e312f7dbc
Комков Алексей <ale45883412@yandex.ru>
2023-11-27 19:29:18 +0700

    1

--- backend/run_app.sh
+++ backend/run_app.sh
@@ -4,5 +4,10 @@ while ! nc -z db 5432;
 done;
     echo "connected to the database";
 
+python manage.py migrate recipes;
+python manage.py migrate users;
+python manage.py migrate;
+python manage.py import_ingredients_data;
+python manage.py import_tags_data;
 python manage.py collectstatic --noinput;
 gunicorn -b 0:8000 foodgram_backend.wsgi;

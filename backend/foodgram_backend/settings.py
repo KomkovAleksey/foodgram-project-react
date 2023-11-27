3eadcef4aa1e8c83e3f1fd46b5a819575fceb498
Комков Алексей <ale45883412@yandex.ru>
2023-11-27 14:20:38 +0700

    31234

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -152,10 +152,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static/')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media/')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
